c2ccfaa24a9393ce80a9068f09f4e75a53752584
SharifAli645 <pandemiaonirica@gmail.com>
2023-05-30 12:39:13 -0700

    new py-script: 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-#str = str[str.find('Python'):str.find(' ',str.find('Python'))]
-str = str[str.find("P"):str.find("n")+1]
+str = str[39:67] + str[115:120] + str[:6]
 print(str)
