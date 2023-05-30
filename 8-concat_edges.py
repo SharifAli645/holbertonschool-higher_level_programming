d4886d208e9ba18fe25024457136e7a50d18979e
SharifAli645 <pandemiaonirica@gmail.com>
2023-05-30 12:50:22 -0700

    delete

--- 8-concat_edges.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax"
-#str = str[str.find('Python'):str.find(' ',str.find('Python'))]
-str = str[str.find("P"):str.find("n")+1]
-print(str)
