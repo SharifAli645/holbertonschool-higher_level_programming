20c5bbf9970208003a37bc47c17e2140746fb821
SharifAli645 <pandemiaonirica@gmail.com>
2023-05-30 13:04:08 -0700

    fit: evaluate negative number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 == 0:
-    print(f"Last digit of {number} is {number % 10} and is 0")
-elif number % 10 > 5:
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
+l_digit = number % 10
+
+if l_digit == 0:
+    print(f"Last digit of {number} is {l_digit} and is 0")
+elif l_digit > 5:
+    print(f"Last digit of {number} is {l_digit} and is greater than 5")
 else:
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
