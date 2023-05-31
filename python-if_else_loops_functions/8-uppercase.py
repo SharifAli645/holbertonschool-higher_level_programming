08f81bf581083634bc7e4158ab54c8dde80e7c6a
SharifAli645 <pandemiaonirica@gmail.com>
2023-05-31 13:50:02 -0700

    fit: if a character is a letter

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
         cl = ""
         if letter == len(str) - 1:
             cl = "\n"
-        if ord(str[letter]) > 96:
+        if ord(str[letter]) > 96 and ord(str[letter]) < 123:
             print("{}".format(chr(ord(str[letter]) - 32)), end=cl)
         else:
             print("{}".format(str[letter]), end=cl)
