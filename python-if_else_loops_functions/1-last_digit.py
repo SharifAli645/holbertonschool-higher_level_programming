2d9a23f6bda79d1588d1f091bb2dead532dce78e
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-02 14:35:21 -0700

    negative operand

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    n = (number * -1) % 10
+    n = number % -10
 else:
     n = number % 10
 
@@ -11,6 +11,4 @@ if n == 0:
 elif n > 5:
     print(f"Last digit of {number} is {n} and is greater than 5")
 elif n < 6 and n != 0:
-    if number < 0:
-        n = n * -1
     print(f"Last digit of {number} is {n} and is less than 6 and not 0")
