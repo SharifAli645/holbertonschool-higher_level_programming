662f4ee81137c60262750aa0ae02b2a5a7dc3f7c
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-03 19:20:35 -0700

    new program

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    counter = 0
+    if len(sys.argv) == 1:
+        pass
+    else:
+        for i in range(1, len(sys.argv)):
+            counter += int(sys.argv[i])
+    print("{}".format(counter))
