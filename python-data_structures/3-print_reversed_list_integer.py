55118b2fce5a632dcf6eebfb9a89fbc55ad55188
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-05 10:23:23 -0700

    fit: integer format

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for elem in range(-1, (len(my_list) * -1) - 1, -1):
-        print("{}".format(my_list[elem]))
+        print("{:d}".format(my_list[elem]))
