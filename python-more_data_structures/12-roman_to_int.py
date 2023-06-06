eac9c8dba005b60bcfea5efdf3881071040fb8e6
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-06 12:51:06 -0700

    new function

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
+             "M": 1000}
+    cnt = 0
+    for ele in roman_string:
+        if roman.get(ele) > cnt:
+            cnt = roman.get(ele) - cnt
+        else:
+            cnt = cnt + roman.get(ele)
+    return cnt
