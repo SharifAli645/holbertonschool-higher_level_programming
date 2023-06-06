1df0f308833a072b43e822bc44a26a719453561f
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-06 11:59:10 -0700

    fit :  only one list

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    num = [number]
-    num = num * len(my_list)
-    new = map(lambda x, y: x * y, my_list, num)
+    new = map(lambda x: x * number, my_list)
     return list(new)
