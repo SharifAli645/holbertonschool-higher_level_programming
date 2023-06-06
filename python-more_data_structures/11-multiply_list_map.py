bfc590c21f63e801ee1e3be6c5e312b616d0b422
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-06 11:56:33 -0700

    fit: use lambda

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-def multy(a, b):
-    return a * b
-
-
 def multiply_list_map(my_list=[], number=0):
-    num = []
-    for i in range(len(my_list)):
-        num.append(number)
-    new = map(multy, my_list, num)
+    num = [number]
+    num = num * len(my_list)
+    new = map(lambda x, y: x * y, my_list, num)
     return list(new)
