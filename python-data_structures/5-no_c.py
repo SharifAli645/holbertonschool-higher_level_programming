f3e3a8880a96cf5f3ee7839d3c3241510d2b64e4
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-07 16:39:58 -0700

    fit: use a string like a list

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = []
-    for ch in my_string:
-        new_str.append(ch)
+    new_str = ""
     ct = 0
-    while ct < len(new_str):
-        if new_str[ct] == 'c' or new_str[ct] == 'C':
-            new_str.pop(ct)
-            ct = 0
-        ct += 1
+    for ch in my_string:
+        if ch == 'c' or ch == 'C':
+            continue
+        else:
+            new_str += ch
     return new_str
