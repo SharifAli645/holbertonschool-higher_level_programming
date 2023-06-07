9912a244dc0065c27fea67b5784667bf770f5dd2
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-07 16:29:38 -0700

    fit: validate list and use copy funciton

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = list()
-    for ele in my_list:
-        new_list.append(ele)
-    if idx < 0 or len(my_list) < idx:
-        pass
-    else:
-        new_list[idx] = element
-    return new_list
+    if my_list:
+        new_list = my_list.copy()
+        if idx < 0 or len(my_list) < idx:
+            pass
+        else:
+            new_list[idx] = element
+        return new_list
