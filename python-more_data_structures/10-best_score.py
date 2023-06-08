f122d4a6061fc92255abbfaaf16329a74bda913a
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-07 17:00:11 -0700

    fit:return None if there is not score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    keys = a_dictionary.keys()
-    mx = -9999
-    pr = ""
-    for ele in keys:
-        if a_dictionary.get(ele) > mx:
-            mx = a_dictionary.get(ele)
-            pr = ele
-    return pr
+    player = None
+    if a_dictionary:
+        keys = a_dictionary.keys()
+        mx = -1
+        for ele in keys:
+            if a_dictionary.get(ele) > mx:
+                mx = a_dictionary.get(ele)
+                player = ele
+    return player
