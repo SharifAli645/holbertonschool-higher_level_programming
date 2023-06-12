c4aefd959c8e648de84114fc3cdcea7016072dcc
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-11 18:42:37 -0700

    fit: reversed list

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,11 @@ def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
              "M": 1000}
     cnt = 0
-    for ele in roman_string:
-        if roman.get(ele) > cnt:
-            cnt = roman.get(ele) - cnt
+    last = 0
+    for ele in reversed(roman_string):
+        if roman.get(ele) >= last:
+            cnt = roman.get(ele) + cnt
+            last = roman.get(ele)
         else:
-            cnt = cnt + roman.get(ele)
+            cnt = cnt - roman.get(ele)
     return cnt
