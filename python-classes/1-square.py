bfd56e4242e8ea81835f5787eda02fc89dc25560
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-13 11:58:49 -0700

    correct documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""Class that define a geometric figure"""
+
+
 class Square:
+    """
+    Represents a square.
+    ...
+
+    Attributes
+    ----------
+    size : int
+        size of the square
+
+    """
     def __init__(self, size):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+            size : int
+                size of the square
+        """
         self.__size = size
