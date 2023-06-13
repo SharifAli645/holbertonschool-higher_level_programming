0f2bc43d15de0bd9ce4efbdcb9636dac1d891a62
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-13 12:12:02 -0700

    correct documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
+"""Class that define a geometric figure"""
+
+
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        size (int): size of the square.
+    """
     def __init__(self, size=0):
+        """
+        Constructor.
+
+        Args:
+            size (int): size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
