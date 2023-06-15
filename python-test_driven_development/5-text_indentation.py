72a10d2f9da2217979e77dee015f5fb84a658f4b
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-14 17:55:28 -0700

    feat: evaluate argument

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""To manipulate strings"""
+
+
 def text_indentation(text):
+    """print blankets"""
+    if type(text) not in [str]:
+        raise TypeError("text must be a string")
     for char in text:
         print("{}".format(char), end="")
         if char in ['.', '?', ':']:
-            print("\n")
+            print("\n\n", end="")
