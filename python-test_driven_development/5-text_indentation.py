83d5d2b2bf54a6cbbb15a4566d71904c80170d2c
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-14 18:24:21 -0700

    fit: without space at the beginning

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,9 +4,15 @@
 
 def text_indentation(text):
     """print blankets"""
+    last = ""
     if type(text) not in [str]:
         raise TypeError("text must be a string")
     for char in text:
+        if last in ['.', '?', ':']:
+            last = ""
+            continue
         print("{}".format(char), end="")
         if char in ['.', '?', ':']:
+            last = char
             print("\n\n", end="")
+            continue
