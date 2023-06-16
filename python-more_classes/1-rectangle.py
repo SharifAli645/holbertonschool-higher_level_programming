1a58d2a6fe4fe773e79bb0bd307257e8628dd429
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-16 11:01:30 -0700

    different comparation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
