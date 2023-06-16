0ba685aed433f13af3004dfbcc38e914e017a7c5
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-15 20:37:45 -0700

    new class

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Represent geometric figures'''
+
+
+class Rectangle():
+    '''Represent a rectangle'''
+    def __init__(self, height=0, width=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) not in [int]:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) not in [int]:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
