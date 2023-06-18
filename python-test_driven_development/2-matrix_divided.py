cdededb7c010f8dce5096f8753eb0a199ae16467
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-18 11:39:23 -0700

    reformat function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     msg1 = "matrix must be a matrix (list of lists) of integers/floats"
-    if(type(matrix) not in [list] or type(matrix[0]) not in [list]
-            or type(matrix[1]) not in [list]):
+    msg2 = "Each row of the matrix must have the same size"
+    if type(matrix) != list:
         raise TypeError(msg1)
-    for element in matrix[0]:
-        if type(element) not in [int, float]:
+    for x in range(len(matrix)):
+        if type(matrix[x]) != list:
             raise TypeError(msg1)
-
-    for element in matrix[1]:
-        if type(element) not in [int, float]:
-            raise TypeError(msg1)
-
-    if len(matrix[0]) != len(matrix[1]):
-        raise TypeError("Each row of the matrix must have the same size")
+        if x != len(matrix) - 1:
+            if len(matrix[x]) != len(matrix[x + 1]):
+                raise TypeError(msg2)
+        for k in range(len(matrix[x])):
+            if type(matrix[x][k]) not in [int, float]:
+                raise TypeError(msg1)
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    c_matrix = []
-    for lst in range(len(matrix)):
-        c_matrix.append([])
-        for ele in range(len(matrix[lst])):
-            c_matrix[lst].append(round(matrix[lst][ele] / div, 2))
-    return c_matrix
+    new_l = matrix.copy()
+    for i in range(len(matrix)):
+        new_l[i] = list(map(lambda x: round(x / div, 2), matrix[i]))
+    return new_l
