dd81e5c0e35e21c32f150f91721c2710044a5375
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-18 07:28:01 -0700

    correct private variable

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -24,8 +24,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
