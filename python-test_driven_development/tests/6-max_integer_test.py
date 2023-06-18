154ffd1474902491ddbce9985ea1962db84daea7
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-18 10:36:51 -0700

    more cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,3 +14,4 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer([-999, -1, -440, -10]), -1)
         self.assertAlmostEqual(max_integer([-9.5, 82.6, 97.96, 97.97]), 97.97)
+        self.assertAlmostEqual(max_integer([True, False]), True)
