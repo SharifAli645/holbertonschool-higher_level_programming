4cc7ab76442e81da62734480fc82bddb79eef3ef
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-19 15:26:06 -0700

    new function

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module
+that handles
+Files
+"""
+
+
+def read_file(filename=""):
+    """
+    Function that print the content of a file
+
+    Args:
+        filename (string): the name's file
+    """
+    fle = open(filename)
+    print(fle.read(), end="")
+    fle.close()
