4a384a6fc636025d0e9a337ab8bccd992b8d3200
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-19 12:34:38 -0700

    new class

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Show the methods
+and
+attributes of an object
+"""
+
+
+def lookup(obj):
+    """Function that show the methods and attributes
+    of and object
+    """
+    return dir(obj)
