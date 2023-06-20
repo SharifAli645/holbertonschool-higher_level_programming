b65be4ca073241cda382f9c1d0ba1c03b5cfc4a9
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-20 14:57:16 -0700

    new class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class
+that
+is empty
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Function that raise a error
+
+        Raise:
+            Exception:
+        """
+        raise Exception("area() is not implemented")
