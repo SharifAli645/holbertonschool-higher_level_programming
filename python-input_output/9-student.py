81b1b761d52353327a5645ec5ff10e838d161772
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-23 19:41:52 -0700

    new class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module
+that defines
+a Student
+"""
+
+
+class Student:
+    """Class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor
+
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Function that retrives a dictionary representation
+        of a Student instance
+
+        Returns: dictionary representation
+        """
+        return self.__dict__
