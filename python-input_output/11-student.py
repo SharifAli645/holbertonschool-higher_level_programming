475379da3070f9f4eba7b0d870f1de303386a498
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-25 16:00:47 -0700

    new class

---
+++ python-input_output/11-student.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Module
+that defines
+a Student
+"""
+
+
+class Student:
+    """Class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor
+
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Function that retrives a dictionary representation
+        of a Student instance
+
+        Returns: dictionary representation
+        """
+        if attrs is None:
+            atrb = self.__dict__
+        else:
+            di = dict()
+            for ele in attrs:
+                if ele in self.__dict__:
+                    di[ele] = self.__dict__.get(ele)
+            atrb = di
+        return atrb
+
+    def reload_from_json(self, json):
+        """
+        Function that replaces all attributes of the Student instance
+
+        Args:
+            json (dictionary): attributes of an instance
+        """
+        self.first_name = json.get("first_name")
+        self.last_name = json.get("last_name")
+        self.age = age = json.get("age")
