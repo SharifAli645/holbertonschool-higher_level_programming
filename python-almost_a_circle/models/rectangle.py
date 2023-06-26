c7931f7c6b7f7faf93135484636d5fca25f146be
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-26 13:50:18 -0700

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,16 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(' ' * self.__x, end='')
             print('#' * self.__width)
+
+    def update(self, *args):
+        """Function that updates attributes of an instance"""
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.__width = args[1]
+        if len(args) == 3:
+            self.__height = args[2]
+        if len(args) == 4:
+            self.__x = args[3]
+        if len(args) == 5:
+            self.__y = args[4]
