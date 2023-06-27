eaec5b7ae60955dfe4180028e1c02f7d142bc4bc
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-27 09:14:24 -0700

    update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,3 +23,17 @@ class Square(Rectangle):
         """Overriding the str method"""
         return f"""[{self.__class__.__name__}] ({self.id}) \
 {self.x}/{self.y} - {self.width}"""
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter function of size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
