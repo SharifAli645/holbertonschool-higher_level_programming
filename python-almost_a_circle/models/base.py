8dfe4ad6cdf1fe0f0f26da6dc82a0808ab838fc1
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-27 10:38:03 -0700

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,6 +29,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Static method that returns a JSON string"""
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
