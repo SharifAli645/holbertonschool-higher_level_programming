3fce770ee82fd2e7d02728a485942f645c5359da
SharifAli645 <pandemiaonirica@gmail.com>
2023-06-27 08:58:17 -0700

    new class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module of
+classes that
+define geometric
+figures
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that defines
+    a geometric figure
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Constructor of Square """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overriding the str method"""
+        return f"""[{self.__class__.__name__}] ({self.id}) \
+{self.x}/{self.y} - {self.width}"""
