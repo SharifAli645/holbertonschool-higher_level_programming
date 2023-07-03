6187a0554f3b080fc7062ef7c8301a696230c675
SharifAli645 <pandemiaonirica@gmail.com>
2023-07-02 21:51:46 -0700

    update

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -30,34 +30,36 @@ class TestBase(unittest.TestCase):
                                              'height': 5}]), j_str)
 
     def test_save_to_file(self):
-        s1 = Square(10, 5, 1)
-        s2 = Square(4)
-        dicty = [s1.to_dictionary(), s2.to_dictionary()]
-
-        Square.save_to_file([])
-        with open("Square.json", "r") as f:
-            self.assertEqual("[]", f.read())
-
-        Rectangle.save_to_file([])
-        with open("Rectangle.json", "r") as f:
-            self.assertEqual("[]", f.read())
-
-        r1 = Rectangle(10, 5, 1, 12)
-        r2 = Rectangle(4, 6)
-        dicty = [r1.to_dictionary(), r2.to_dictionary()]
-
-        Rectangle.save_to_file([r1, r2])
-        with open("Rectangle.json", "r") as f:
-            j_dicty = json.loads(f.read())
-        self.assertTrue(dicty == j_dicty)
-
+        """Test for this method"""
         Rectangle.save_to_file(None)
         with open("Rectangle.json", "r") as f:
-            self.assertEqual("[]", f.read())
-
+            self.assertAlmostEqual(f.readline(), '[]')
+        r3 = Rectangle(10, 7, 2, 8)
+        r4 = Rectangle(2, 4)
+        Rectangle.save_to_file([r3, r4])
+        fi1 = r3.to_dictionary()
+        fi2 = r4.to_dictionary()
+        filec = [fi1, fi2]
+        fi3 = Rectangle.to_json_string(filec)
+        with open("Rectangle.json", "r") as f:
+            self.assertAlmostEqual(f.readline(), fi3)
         Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as f:
-            self.assertEqual("[]", f.read())
+            self.assertAlmostEqual(f.readline(), '[]')
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            self.assertAlmostEqual(f.readline(), '[]')
+        s3 = Square(4, 2, 7)
+        s4 = Square(5)
+        Square.save_to_file([s3, s4])
+        ss1 = s3.to_dictionary()
+        ss2 = s4.to_dictionary()
+        s3 = Square.to_json_string([ss1, ss2])
+        with open("Square.json", "r") as f:
+            self.assertAlmostEqual(f.readline(), s3)
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertAlmostEqual(f.readline(), '[]')
 
     def test_from_json_string(self):
         self.assertEqual(Base.from_json_string(None), [])
