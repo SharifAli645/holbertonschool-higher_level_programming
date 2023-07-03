79b401ba2ce324f3f8db9bee84dc3eb82d5b2cd6
SharifAli645 <pandemiaonirica@gmail.com>
2023-07-02 20:20:53 -0700

    update

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 
-import unittest
+from unittest import TestCase, mock
+from io import StringIO
 import json
 import pycodestyle
-
 from models.base import Base
 from models.rectangle import Rectangle
 
 
-class TestRectangle(unittest.TestCase):
+class TestRectangle(TestCase):
     """Class test Rectangle"""
 
     def test_basic(self):
@@ -53,4 +53,20 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(Rectangle(10, 10).area(), 100)
 
     def test_display(self):
-        pass
+        with mock.patch('sys.stdout', new=StringIO()) as output:
+            Rectangle(3, 2).display()
+            self.assertEqual(output.getvalue(), '###\n###\n')
+
+        with mock.patch('sys.stdout', new=StringIO()) as output:
+            Rectangle(3, 2, 3, 3).display()
+            self.assertEqual(output.getvalue(), '\n\n\n   ###\n   ###\n')
+
+    def test_str(self):
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(4, 8, 16, 32, 64)
+        r2 = Rectangle(5, 5)
+        
+        s1 = r1.__str__()
+        s2 = r2.__str__()
+        self.assertEqual(s1, "[Rectangle] (64) 16/32 - 4/8")
+        self.assertEqual(s2, "[Rectangle] (1) 0/0 - 5/5") 
