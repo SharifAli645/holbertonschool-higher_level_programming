757ad134641b846bf6126f15749f7f4630a86903
SharifAli645 <pandemiaonirica@gmail.com>
2023-07-02 17:17:32 -0700

    update

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,51 +1,49 @@
 import unittest
+import json
+import pep8
 
+from models.base import Base
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
+    """"""
+    def test_basic(self):
+        self.assertRaises(TypeError, Rectangle, None)
+        self.assertRaises(TypeError, Rectangle, 1)
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, 4, 5, 6)
 
-    def test_width(self):
-        r_1 = Rectangle(1, 1)
-        r_2 = Rectangle(2, 3)
-        r_3 = Rectangle(4, 4)
+        self.assertEqual(Rectangle(2, 4).id, 1)
+        self.assertEqual(Rectangle(3, 6).id, 2)
 
-        self.assertEqual(r_1.width, 1)
-        self.assertEqual(r_2.width, 2)
-        self.assertEqual(r_3.width, 4)
+        self.assertEqual(Rectangle(2, 4, 6, 12, 24).id, 24)
+        self.assertEqual(Rectangle(3, 6, 12, 24, 42).id, 42)
 
-    def test_height(self):
-        r_1 = Rectangle(1, 1)
-        r_2 = Rectangle(2, 3)
-        r_3 = Rectangle(4, 4)
+        Base._Base__nb_objects = 0
+        r = Rectangle(10, 20, 30, 40)
+        r.width = 11
+        self.assertEqual(r.width, 11)
+        r.height = 21
+        self.assertEqual(r.height, 21)
+        r.x = 31
+        self.assertEqual(r.x, 31)
+        r.y = 41
+        self.assertEqual(r.y, 41)
 
-        self.assertEqual(r_1.height, 1)
-        self.assertEqual(r_2.height, 3)
-        self.assertEqual(r_3.height, 4)
+    def test_values(self):
+        Base._Base__nb_objects = 0
+        r = Rectangle(20, 20)
+        self.assertRaises(ValueError, Rectangle, -2, 2)
+        self.assertRaises(ValueError, Rectangle, 0, 2)
+        self.assertRaises(ValueError, Rectangle, 2, 2, -3, 2)
 
-    def test_x(self):
-        r_1 = Rectangle(1, 1)
-        r_2 = Rectangle(2, 3)
-        r_3 = Rectangle(4, 4)
+    def test_types(self):
+        self.assertRaises(TypeError, Rectangle, "2", 2)
+        self.assertRaises(TypeError, Rectangle, 2, "2")
+        self.assertRaises(TypeError, Rectangle, 2, 2, "2", 2)
+        self.assertRaises(TypeError, Rectangle, 2, 2, 2, "2")
 
-        self.assertEqual(r_1.x, 0)
-        self.assertEqual(r_2.x, 0)
-        self.assertEqual(r_3.x, 0)
-
-    def test_y(self):
-        r_1 = Rectangle(1, 1)
-        r_2 = Rectangle(2, 3)
-        r_3 = Rectangle(4, 4)
-
-        self.assertEqual(r_1.y, 0)
-        self.assertEqual(r_2.y, 0)
-        self.assertEqual(r_3.y, 0)
-
-    def test_area(self):
-        r_1 = Rectangle(1, 1)
-        r_2 = Rectangle(2, 3)
-        r_3 = Rectangle(4, 4)
-
-        self.assertEqual(r_1.area(), 1)
-        self.assertEqual(r_2.area(), 6)
-        self.assertEqual(r_3.area(), 16)
+    def test_pep8(self):
+        """checks pep 8"""
+        p8 = pep8.StyleGuide(quiet=True)
+        outcome = p8.check_files(['tests/test_models/test_rectangle.py']) 
